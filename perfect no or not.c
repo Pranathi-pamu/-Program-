da3296d4d716c75639ad8815a8c870913d7175ff
Pranathi-pamu <chinnupamu11@gmail.com>
2024-10-14 15:24:52 +0530

    Add files via upload

---
+++ perfect no or not.c
@@ -0,0 +1,50 @@
+#include <math.h>
+
+int main() 
+{
+     int  n,i,s=0;
+     printf("enter the  n value ");
+     scanf("%d",&n);
+     for(i=1;i<=n/2;i++)
+     { 
+     n%i==0;
+     s=s+i;
+     
+     
+     }
+     if(n==s)
+     
+     printf("perfect no");
+     else 
+     printf("not a perfect no");
+     
+     
+     
+     
+     
+     
+    
+  
+ 
+       
+         
+       
+         
+       
+       
+     
+     
+      
+      
+      
+      
+      
+     
+     
+      
+ }    
+ 
+     
+
+
+    
